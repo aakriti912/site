b6e3d7e1f77a5a119d70d4321fb83ffc9eee4b9c
Aakriti <thapaaakriti570@gmail.com>
2020-05-22 19:05:51 +0545

    my project

---
+++ script.js
@@ -0,0 +1,28 @@
+//function ShowDate(){
+//	document.getElementById('datep').innerHTML = Date();
+//}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+			//for font and color
+			document.querySelector('#red').onclick = function() {
+			document.querySelector('#WEBSITE').style.color= 'red';
+				// body...
+			};
+			document.querySelector('#blue').onclick= function() {
+				document.querySelector('#WEBSITE').style.color= 'blue';
+			};
+
+			document.querySelector('#yellow').onclick= function() {
+				document.querySelector('#WEBSITE').style.color= 'yellow';
+			};
+			// body...
+		});
+
+
+function ShowDate(){
+document.getElementById('datep').innerHTML = Date();
+}
+
+//array
+
